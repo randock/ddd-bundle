87db094632260c612ea8dd598205e65e3a6de10e
Diego Hervas <diego.diguin91@gmail.com>
2018-04-17 17:11:17 +0200

    Added TacticianMiddleware Service

---
+++ src/DependencyInjection/dddBundleExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Randock\Infrastructure\CategoryBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+/**
+ * Class RandockInfrastructureCategoryExtension.
+ */
+class dddBundleExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
