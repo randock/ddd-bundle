885aa25c7c8c3895b31fa5002dbede56d2b4e102
Diego Hervas <diego.diguin91@gmail.com>
2018-04-18 09:37:34 +0200

    Ant fix

--- src/RandockDddBundle.php
+++ src/RandockDddBundle.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Randock\DddBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
- * Class RandockDddBundle
+ * Class RandockDddBundle.
  */
 class RandockDddBundle extends Bundle
 {
