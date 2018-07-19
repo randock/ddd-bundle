4eb2c253b9c245f85180c55c48528ba8933fe5ea
Juanjo <juanjose@randock.com>
2018-07-19 14:31:18 +0200

    cs fixer

--- src/Infrastructure/Doctrine/Types/UTCDateTimeType.php
+++ src/Infrastructure/Doctrine/Types/UTCDateTimeType.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Randock\DddBundle\Infrastructure\Doctrine\Types;
 
-
 use Doctrine\DBAL\Types\DateTimeType;
 use Doctrine\DBAL\Types\ConversionException;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
